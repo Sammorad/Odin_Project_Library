efb8bd316246e5ef67bdce7ac7fa641bedd23c36
dejiowoeye <50265769+Sammorad@users.noreply.github.com>
2025-11-28 12:18:31 +0100

    Finished the library app

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
-const myLibrary = []; // An array of books in the library 
+let myLibrary = []; // An array of books in the library 
 const tableBody = document.querySelector("tbody");
 const newBook = document.querySelector(".new-bk");
+const viewBook = document.querySelector(".view")
 
 
-function Book(name, author, number_of_pages, read){
+function Book(name, author, number_of_pages,read){
     //the constructor for Books to be added to the library//
     this.ID = crypto.randomUUID()
     this.name = name;
     this.author = author;
     this.number_of_pages = number_of_pages;
-    this.read = read;
+    this.read = read
 }
 
 
@@ -19,25 +20,37 @@ function addBook (item){
     return item 
       
 }
-
+const ifReadBtn = document.createElement("button");//created a button to assert if the book as been read//
+        ifReadBtn.textContent = "Book read"
 function dispTable(myLibrary, tableBody){
     //displays books in the array with the table//
     tableBody.innerHTML = " " //to clear the table and prevent dublication 
     
     for (let book of myLibrary){
         const row = document.createElement("tr");
+        const deleteBtn = document.createElement("button");
+               
+        deleteBtn.textContent = "Delete";
+        deleteBtn.dataset.id = book.ID;
+
         row.innerHTML = `
-        <td>${book.ID}</td>
+        <td>${(myLibrary.indexOf(book) + 1)}</td>
         <td>${book.name}</td>
         <td>${book.author}</td>
         <td>${book.number_of_pages}</td>
-        <td>${book.read}</td>`
+        <td>${book.read}</td>
+        `//delete individual items from the myLibrary//
+        const del_td = document.createElement("td")
+        del_td.appendChild(deleteBtn);
+        row.appendChild(del_td)
         tableBody.appendChild(row)
+        
 
 
-    }
+    }   
     return tableBody
 }
+
 //form to take inputs when users add Books to myLibrary
 const form = document.createElement("form")
 form.innerHTML = `<fieldset>
@@ -47,41 +60,66 @@ form.innerHTML = `<fieldset>
                     <input type="text" id="Author" name="author"></input>
                     <label for ="pages" >Number of Pages</label>
                     <input type="number" id="pages" name="number_of_pages"></input>
-                    <label for="read"  >Read</label>
-                    <input id="read" type="radio" value="yes" name="read"></input>
+                    <label for = "read"> Read<\label>
+                    <input type = "checkbox" id = "read" name = "read">
                     <button>Submit</button>
                     
                     
                 </fieldset>`
                 
 
-const formDisp = document.querySelector(".lib-form"); //div containing forms imported to append child element//
+const dispForm = document.querySelector(".forms")//div to attach forms when displayed 
 //newBook button to callup form when clicked 
 newBook.addEventListener("click", ()=>{
-    formDisp.appendChild(form)
+    dispForm.appendChild(form)
     form.style.display ="block";
     
 })
 form.addEventListener("submit", (event)=>{
+    //this function adds book to library on click of the submit button
     event.preventDefault()
     const name01 = form.Book_title.value;
     const author01 = form.Author.value;
     const pages01 = form.pages.value;
-    const read01 = form.read.value
-    const item = new Book(name01, author01, pages01, read01)
+    const readStatus = form.read.checked;
+ 
+
+    const item = new Book(name01, author01, pages01, readStatus)
     addBook(item)
-    dispTable(myLibrary, tableBody)
+
+    //also check the read checkbox if clicked 
+   
+
+    
 
 })
 
-    
-const item1 = new Book("AYod", "you", 54, true)
-const item2 = new Book("Tedi", "Tat", 56, false)
+viewBook.addEventListener("click", ()=>{
+    //to display the content of myLibrary in table format//
+    dispTable(myLibrary, tableBody)
+       
+})
 
 
-addBook(item1)
-addBook(item2)
+//Event listener for delete button to delete books via clicks//
+tableBody.addEventListener("click", (event)=>{
+    if (event.target.textContent === "Delete"){
+        const bookID = event.target.dataset.id;
+        myLibrary = myLibrary.filter(book => book.ID !== bookID);
+        dispTable(myLibrary, tableBody) //Render the table 
+    }
+
+})
 
 
-console.log(myLibrary) 
+
+
+
+
+
+
+
+
+
+
 
